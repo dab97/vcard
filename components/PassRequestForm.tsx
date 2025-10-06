624047c20dffdf3a3b600a0f4b275b44b562ba7d
dab97 <68206308+dab97@users.noreply.github.com>
2025-10-06 18:38:17 +0300

    update

--- components/PassRequestForm.tsx
+++ components/PassRequestForm.tsx
@@ -509,7 +509,7 @@ function PassRequestFormContent() {
             </div>
 
             {isListening && isMobile && (
-              <div className="fixed inset-0 flex items-center justify-center bg-background/60 backdrop-blur-xs z-50">
+              <div className="fixed inset-0 flex items-center justify-center bg-foreground/90 backdrop-blur-xs z-50">
                 <div className="flex flex-col items-center p-4 rounded-lg">
                   <Mic className="h-24 w-24 text-white animate-pulse-mic" />
                   <p className="mt-4 text-white text-lg text-center">
@@ -521,7 +521,7 @@ function PassRequestFormContent() {
                     className="mt-4"
                     onClick={stopListening}
                   >
-                    <StopCircle className="h-5 w-5 mr-2" />
+                    <StopCircle className="h-8 w-8 mr-2" />
                     Закончить запись
                   </Button>
                 </div>
