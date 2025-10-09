0b992b272b8aa7e4945d3e5f693bf8c375fe68c6
idab <thedab97@gmail.com>
2025-10-09 12:29:19 +0300

    Config: Set Next.js output to standalone

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const nextConfig: NextConfig = {
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
   serverExternalPackages: ["@sparticuz/chromium", "puppeteer-core"],
+  output: 'standalone', // Добавляем для оптимизации развертывания на Vercel
 };
 
 export default nextConfig;
